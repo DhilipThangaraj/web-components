ad42d3243adba84d4825a08f597cb9af13125481
Dhilipkumar Thangaraj <dhilipthangaraj777@gmail.com>
2022-12-14 17:22:57 +0400

    finishing up the user card web component - final piece

--- userCard.js
+++ userCard.js
@@ -41,10 +41,36 @@ template.innerHTML = `
 class UserCard extends HTMLElement {
   constructor() {
     super();
+    this.showInfo = true;
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.shadowRoot.querySelector("h3").innerText = this.getAttribute("name");
     this.shadowRoot.querySelector("img").src = this.getAttribute("avatar");
+  }
+
+  toggleInfo() {
+    this.showInfo = !this.showInfo;
+
+    const info = this.shadowRoot.querySelector(".info");
+    const toggleButton = this.shadowRoot.querySelector("#toggle-info");
+
+    if (this.showInfo) {
+      info.style.display = "block";
+      toggleButton.innerText = "Hide Info";
+    } else {
+      info.style.display = "none";
+      toggleButton.innerText = "Show Info";
+    }
+  }
+
+  connectedCallback() {
+    this.shadowRoot
+      .querySelector("#toggle-info")
+      .addEventListener("click", () => this.toggleInfo());
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot.querySelector("#toggle-info").removeEventListener();
   }
 }
 
